85d1eddc15a841def855eebdb0a766042dd2792a
Thasyo Peres <thasyotp2002@gmail.com>
2023-01-20 03:18:13 -0300

    add update project

---
+++ js/productsAllCarousel.js
@@ -0,0 +1,51 @@
+$( () => {
+    $(".carousel-decoration").slick({
+        infinite: true,
+        slidesToShow: 3,
+        speed: 500,
+        slidesToScroll: 1,
+        centerMode: true,
+        prevArrow: $(".prev-arrow-decoration"),
+        nextArrow: $(".next-arrow-decoration"),
+        responsive: [
+            {
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 1,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                variableWidth: true,
+              }
+            }
+          ]
+    });
+})
+
+$( () => {
+    $(".carousel-stationarystore").slick({
+        infinite: true,
+        slidesToShow: 3,
+        speed: 500,
+        slidesToScroll: 1,
+        centerMode: true,
+        prevArrow: $(".prev-arrow-stationarystore"),
+        nextArrow: $(".next-arrow-stationarystore"),
+        responsive: [
+            {
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 1,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                variableWidth: true,
+              }
+            }
+          ]
+    });
+})
