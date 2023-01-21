1775d9a004934260c02b2943990a05794d6521b6
Thasyo Peres <thasyotp2002@gmail.com>
2023-01-21 17:40:41 -0300

    add update productsAll.html

--- js/productsAllCarousel.js
+++ js/productsAllCarousel.js
@@ -83,3 +83,59 @@ $( () => {
         ]
   });
 })
+
+//ToysCarrossel
+$( () => {
+  $(".carousel-toys").slick({
+      infinite: true,
+      slidesToShow: 5,
+      speed: 500,
+      slidesToScroll: 1,
+      centerMode: true,
+      prevArrow: $(".prev-arrow-toys"),
+      nextArrow: $(".next-arrow-toys"),
+      responsive: [
+          {
+            breakpoint: 1025,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              variableWidth: true,
+            }
+          }
+        ]
+  });
+})
+
+//AccessoriesCarousel
+$( () => {
+  $(".carousel-accessories").slick({
+      infinite: true,
+      slidesToShow: 5,
+      speed: 500,
+      slidesToScroll: 1,
+      centerMode: true,
+      prevArrow: $(".prev-arrow-accessories"),
+      nextArrow: $(".next-arrow-accessories"),
+      responsive: [
+          {
+            breakpoint: 1025,
+            settings: {
+              slidesToShow: 3,
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              variableWidth: true,
+            }
+          }
+        ]
+  });
+})
